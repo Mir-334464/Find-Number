fcda246c42c1125421f38c4128233b8290c92719
Mir-334464 <106953464+Mir-334464@users.noreply.github.com>
2022-06-27 16:13:32 +0530

    Add files via upload

---
+++ Frequency.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Frequency
+
+  {
+
+     public static void main(String[]args)
+
+       {
+
+        Scanner sc=new Scanner(System.in);
+      
+        System.out.println("Enter Any Number");
+       
+        int N=sc.nextInt();
+
+       System.out.println("Enter Number You Want To Find & Count");
+       
+        int num=sc.nextInt();
+
+        int rem=0; int Count=0;
+ 
+while(N!=0)
+{
+
+  rem=N%10;
+
+ if(rem==num)
+ Count++;
+
+N=N/10;
+
+}
+
+
+if(Count>0)
+  {
+System.out.println("Number Found :" +num);
+System.out.println(+num+" is"+Count+ " Times in Given Number");
+}
+else
+System.out.println("Number Not Found :" +num);
+
+    }
+}
+
+
+
